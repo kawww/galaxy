8220d90ed463cc7cfc6ad0fbc10a888327f40966
kawww <39939137+kawww@users.noreply.github.com>
2020-03-22 05:37:45 +0800

    fix links

--- rpc.php
+++ rpc.php
@@ -340,33 +340,16 @@ function http_post_json($url, $jsonStr)
 }
 
 
-function turnUrlIntoHyperlink($string){
-    //The Regular Expression filter
-    $reg_exUrl = "/(?i)\b((?:https?:\/\/|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))/";
+function turnUrlIntoHyperlink($text){
 
-    // Check if there is a url in the text
-    if(preg_match_all($reg_exUrl, $string, $url)) {
-
-        // Loop through all matches
-        foreach($url[0] as $newLinks){
-            if(strstr( $newLinks, ":" ) === false){
-                $link = 'http://'.$newLinks;
-            }else{
-                $link = $newLinks;
-            }
-
-            // Create Search and Replace strings
-            $search  = $newLinks;
-
-			//$link=str_replace("https://","",$link);
-            $replace = '<a href="'.$link.'" target=_blank>'.$link.'</a>';
-		
-            $string = str_replace($search, $replace, $string);
-        }
-    }
-
-    //Return result
-    return $string;
+ $text = eregi_replace('(((f|ht){1}tp://)[-a-zA-Z0-9@:%_+.~#?&//=]+)',
+ '<a href="\1">\1</a>', $text);
+ $text = eregi_replace('([[:space:]()[{}])(www.[-a-zA-Z0-9@:%_+.~#?&//=]+)',
+ '\1<a href="http://\2">\2</a>', $text);
+ $text = eregi_replace('([_.0-9a-z-]+@([0-9a-z][0-9a-z-]+.)+[a-z]{2,3})',
+ '<a href="mailto:\1">\1</a>', $text);
+          
+return $text;
 }
 
 
