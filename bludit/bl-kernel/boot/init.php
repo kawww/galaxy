9ecfe2ee9d6497e9a126cd58ca78170706c79e1b
kawww <39939137+kawww@users.noreply.github.com>
2020-07-06 05:48:06 +0800

    Add files via upload

--- bludit/bl-kernel/boot/init.php
+++ bludit/bl-kernel/boot/init.php
@@ -212,7 +212,7 @@ define('MARKDOWN_PARSER', $site->markdownParser());
 
 $checktheme=$_REQUEST["theme"];
 
-if(!$checktheme){$checktheme="alternative";}
+if(!$checktheme){$checktheme="social-network";}
 
 // --- PHP paths with dependency ---
 // This paths are absolutes for the OS
